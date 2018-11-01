863171fb303b44eff1c2fb801337e7209a97dc3d
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2018-11-01 23:00:12 +0530

    Created basic Django app

---
+++ Foodezz/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Studies\Sem-3\ITWS-3\Project\Foodezz\Foodezz\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
